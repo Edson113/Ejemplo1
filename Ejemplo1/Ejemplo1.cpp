793a27dc5863a5ef4e9d5efba68328d12d651e58
JRsalinas98 <jerry_salinas98@hotmail.com>
2022-05-12 14:08:50 -0500

    editando ejemplo01

--- Ejemplo1/Ejemplo1.cpp
+++ Ejemplo1/Ejemplo1.cpp
@@ -1,1 +1,40 @@
 //hola
+#include <iostream>
+#include <string>
+
+using namespace std;
+struct Person {
+	string name;
+	int age;
+	double height;
+};
+struct Nodo {
+	Person dato;
+	Nodo* ant;
+	Nodo* sgte;
+};
+void insertarFinal(Nodo*&, Person);
+void listar(Nodo*);
+int main(void) {
+	Nodo* listaDoble = NULL;
+	int cant;
+	Person valor;
+	cout << "LISTA DOBLE" << endl;
+	do {
+		cout << "Cantidad de personas a ingresar: "; cin >> cant;
+	} while (cant <= 0);
+	for (int i = 0; i < cant; i++) {
+		cout << "\n\nDATOS PERSONA [" << i + 1 << "]" << endl;
+		cout << "\nNombre: "; cin.ignore();
+		getline(cin, valor.name);
+		cout << "Edad: ";
+		cin >> valor.age;
+		cout << "Estatura: ";
+		cin >> valor.height; cout << endl;
+
+		insertarFinal(listaDoble, valor);
+	}
+	cout << "\n\n\nElementos de la lista: " << endl;
+	listar(listaDoble);
+	return 0;
+}
